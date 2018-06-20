793bdf72db2bac0d93deffbf46ab32395f4c5ce3
Jackson Taylor <jtaylormuffins@gmail.com>
2018-06-20 09:41:35 -0400

    Added gitignore, started adding templates, able to find media from dirs

--- main.go
+++ main.go
@@ -4,38 +4,94 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-type HomePage struct {
-	Title string
+const (
+	moviesDirectory = "./media/movies/"
+)
+
+type Page struct {
+	Title               string
+	MovieTitleAndSource map[string]string
+}
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("./temp/*.gohtml"))
 }
 
 func main() {
+	moviesFileServer := http.FileServer(http.Dir(moviesDirectory))
 	http.HandleFunc("/", index)
-	//http.HandleFunc("/movies/", movieIndex)
+	http.HandleFunc("/movies/", movieIndex)
+	http.Handle("/movies/files/", moviesFileServer)
 	http.ListenAndServe(":8080", nil)
-
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	//w.Header().Set("Content-Type", "text/html")
-	p := HomePage{Title: "Home"}
+	w.Header().Set("Content-Type", "text/html")
 
-	t, err := template.ParseFiles("./temp/home.html")
-
-	if err != nil {
-		fmt.Println(err)
+	p := Page{
+		Title: "Home",
 	}
 
-	err = t.Execute(w, p)
+	err := tmpl.ExecuteTemplate(w, "header", p)
+	check(err, w)
 
+	err = tmpl.ExecuteTemplate(w, "home.gohtml", nil)
+	check(err, w)
+
+	err = tmpl.ExecuteTemplate(w, "footer", nil)
+	check(err, w)
+}
+
+// /movies handler
+func movieIndex(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+
+	movMap := getMovieInformation(w)
+	p := Page{
+		Title:               "Movies",
+		MovieTitleAndSource: movMap,
+	}
+
+	err := tmpl.ExecuteTemplate(w, "header", p)
+	check(err, w)
+
+	err = tmpl.ExecuteTemplate(w, "movieIndex.gohtml", p)
+	check(err, w)
+
+	err = tmpl.ExecuteTemplate(w, "footer", nil)
+	check(err, w)
+
+}
+
+func check(err error, w http.ResponseWriter) {
 	if err != nil {
 		fmt.Println(err)
 		fmt.Fprintf(w, "%s", err)
 	}
 }
 
-// func movieIndex(w http.ResponseWriter, req *http.Request)  {
-// 	w.Header().Set("Content-type", "text/html")
-//
-// }
+func getMovieInformation(w http.ResponseWriter) map[string]string {
+	m := make(map[string]string)
+
+	err := filepath.Walk(moviesDirectory, func(path string, info os.FileInfo, err error) (er error) {
+		if !info.IsDir() {
+			src := info.Name()
+			name := strings.TrimSuffix(src, filepath.Ext(src))
+			fmt.Println(src)
+			src = "/movies/files/" + src
+			m[name] = src
+		}
+		return nil
+	})
+
+	check(err, w)
+
+	return m
+}
