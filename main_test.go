6fd939355c4c12a026fa9fd5516f2e1c17379bf1
Jackson Taylor <jackson@jacksontaylor.xyz>
2021-02-11 22:58:26 -0500

    Rework tests
    Move to test table approach

--- main_test.go
+++ main_test.go
@@ -1,31 +1,54 @@
 package main
 
 import (
-	_ "reflect"
+	"reflect"
 	"testing"
 )
 
-type fileSystemMock struct{}
-
-var fileExistsMock func(string) bool
+type fileSystemMock struct {
+	fileExistsMock func(string) bool
+}
 
 func (fs fileSystemMock) FileExists(path string) bool {
-	return fileExistsMock(path)
+	return fs.fileExistsMock(path)
 }
 
 func TestReadDirectory(t *testing.T) {
-	testPath := "/incorrectPath/"
-
-	fs := fileSystemMock{}
-
-	// FileSystem can't find the file
-	fileExistsMock = func(_ string) bool {
+	// Mock where fs does not find a file
+	pathDoesNotExist := func(_ string) bool {
 		return false
 	}
 
-	_, err := ReadDirectory(fs, testPath)
-
-	if err == nil {
-		t.Error("Error was not returned!")
+	type args struct {
+		fs   FileSystemInteractor
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "Missing Path",
+			args: args{
+				fs:   fileSystemMock{fileExistsMock: pathDoesNotExist},
+				path: "/incorrectPath",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadDirectory(tt.args.fs, tt.args.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadDirectory() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReadDirectory() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
